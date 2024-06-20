7a6f0414984a8f6cd1677636ff839a6a386fc5c6
hubcad25 <hubertcadieux@gmail.com>
2024-06-20 12:24:50 -0400

    post_demo_diagnose

--- R/usa_post_demo.R
+++ R/usa_post_demo.R
@@ -142,7 +142,7 @@ post_demo_diagnose <- function(
 #' print(plot)
 #'
 #' @export
-graph_post_demo_diagnose <- function(data){
+graph_post_demo_diagnose <- function(data, ridges_scale = 1.25){
   library(ggplot2)
   library(dplyr)
   data <- data %>%
@@ -187,20 +187,21 @@ graph_post_demo_diagnose <- function(data){
     ggridges::geom_density_ridges(aes(y = predicted_class, alpha = wanted_class),
                                   color = NA,
                                   quantile_lines = TRUE, quantiles = 0.5,
-                                  bandwidth = 0.025, scale = 1.5) +
+                                  bandwidth = 0.025, scale = ridges_scale) +
     geom_tile(data = medians, aes(x = median, color = predicted_class),
               width = 0.0025, height = 0.65, show.legend = FALSE) +
     geom_text(data = medians, aes(x = median + 0.005,
                                   color = predicted_class, label = paste0(round(median * 100), "%")),
-              size = 2, hjust = 0, vjust = -0.5, show.legend = FALSE) +
+              size = 2, hjust = 0, vjust = -0.5,
+              fontface = "bold", show.legend = FALSE) +
     clessnize::theme_clean_light() +
     scale_fill_manual(values = colors) +
     scale_color_manual(values = colors) +
-    scale_alpha_continuous(range = c(0.15, 0.6)) +
+    scale_alpha_continuous(range = c(0.15, 0.485)) +
     scale_x_continuous(breaks = seq(from = 0, to = 1, by = 0.1),
                        labels = paste0(seq(from = 0, to = 100, by = 10), "%"),
                        limits = c(0, 1)) +
-    guides(fill = guide_legend(nrow = 2), alpha = "none") +
+    guides(fill = "none", alpha = "none") +
     theme(strip.placement = "outside",
           strip.text.y = element_text(hjust = 0.5, size = 6),
           strip.background = element_rect(fill = "grey90", color = NA),
