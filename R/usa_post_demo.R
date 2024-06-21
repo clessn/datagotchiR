0ccc9feef0e9bf823a94c64c50e5581c0db832c8
hubcad25 <hubertcadieux@gmail.com>
2024-06-21 12:26:48 -0400

    post_demo_diagnose

--- R/usa_post_demo.R
+++ R/usa_post_demo.R
@@ -187,12 +187,12 @@ post_demo_diagnose <- function(
       test_data <- model_data[-train_ix, ] %>%
         mutate(id = 1:nrow(.))
       if (class(models[["vote"]]) == "clm"){
-        preds_vote <- update_and_predict_clm_model(models,
+        preds <- update_and_predict_clm_model(models,
                                                    new_train_data = train_data,
                                                    test_data = test_data) %>%
           dplyr::mutate(iteration = i)
       } else if (class(models[["vote"]]) == "polr") {
-        preds_vote <- update_and_predict_polr_model(models,
+        preds <- update_and_predict_polr_model(models,
                                                     new_train_data = train_data,
                                                     test_data = test_data) %>%
           dplyr::mutate(iteration = i)
